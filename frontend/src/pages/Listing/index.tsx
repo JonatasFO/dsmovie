67b2b2361b904b0c8fa5fe42c5840d56227887e7
Jonatas Ferreira <jonatas_098@hotmail.com>
2022-01-16 16:21:04 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,18 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+  // FORMA ERRADA
+  axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        .then(response => {
+          console.log(response.data)
+        });
+
+
+
   return (
     <>
       <Pagination />
@@ -28,7 +39,7 @@ function Listing() {
           </div>
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
-          </div>
+          </div>  
         </div>
       </div>
 
